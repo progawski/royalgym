611d0eebc1dc52d73ca88de1dd5bd69bc597d57c
progawski <rogawski.piotr@gmail.com>
2020-04-09 14:58:06 +0200

    Switching days in table

--- scripts/classes.js
+++ scripts/classes.js
@@ -1,3 +1,32 @@
 $(document).ready(function(){
-    $(window).scroll(testScroll);
+    var dayOfWeek;
+    $('.btnPreviousDay').click(function(){
+        dayOfWeek = $(this).closest('td').index()+1;
+        console.log(dayOfWeek);
+        dayOfWeek--;
+        if(dayOfWeek < 2){
+            dayOfWeek = 8;
+        }
+        console.log(dayOfWeek);
+        $('table td:not(:nth-child(1)):not(:nth-child('+dayOfWeek+'))').hide();
+        $('table td:nth-child('+dayOfWeek+'):not(.empty)').show();
+    });
+    $('.btnNextDay').click(function(){
+        dayOfWeek = $(this).closest('td').index()+1;
+        console.log(dayOfWeek);
+        dayOfWeek++;
+        if(dayOfWeek > 8){
+            dayOfWeek = 2;
+        }
+        console.log(dayOfWeek);
+        $('table td:not(:nth-child(1)):not(:nth-child('+dayOfWeek+'))').hide();
+        $('table td:nth-child('+dayOfWeek+'):not(.empty)').show();
+    });
+
+    // Animation on scroll configuration
+    AOS.init({
+        once: true,
+        duration: 500
+    }); 
+
 });
